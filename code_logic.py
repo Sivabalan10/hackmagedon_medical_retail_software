b83736ce60324691437e08ecdc8e30ea7d478190
Sriram K <113620147+2Sriram4@users.noreply.github.com>
2024-08-11 09:46:11 +0530

    Add files via upload
    Code login used for weightage assignment.

---
+++ code_logic.py
@@ -0,0 +1,41 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7d075a5a-5b48-4dea-a075-ac3dd4cbc97d",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "\"\"\"\n",
+    "1.Input Parameters: Receive quantity_sold and expiry_date as inputs.\n",
+    "2.Calculate Sales Weightage: Compute weightage as the lesser of quantity_sold / 10 or 10.\n",
+    "3.Determine Expiry Urgency: Calculate expiry urgency based on days until expiry divided by 30.\n",
+    "4.Calculate the Threshold: Compute the threshold using the formula weightage * (1 / (expiry_urgency + 1)).\n",
+    "Interpret the Threshold: A lower threshold suggests a higher urgency to restock.\n",
+    "\"\"\""
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
